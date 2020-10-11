1478ee960f063bcc3bb08b0fc6f2993e29d820cf
JC <68072522+jc-nzc@users.noreply.github.com>
2020-10-11 11:57:57 -0500

    created lists.py

---
+++ lists.py
@@ -0,0 +1,11 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+fruits.count('apple')
+fruits.count('tangerine')
+fruits.index('banana')
+fruits.index('banana', 4)  # Find next banana starting a position 4
+fruits.reverse()
+fruits.append('grape')
+print(fruits)
+fruits.sort()
+print(fruits)
+fruits.pop()
